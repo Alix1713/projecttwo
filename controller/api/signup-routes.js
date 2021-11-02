7ac00124b0f94fd69fedc8fc27eb421d08e46c1a
Leo <leonardodi.gutierrez@gmail.com>
2021-11-02 13:53:19 -0700

    sequelize and api (login/sign up) complete, check if it looks okay?

--- controller/api/signup-routes.js
+++ controller/api/signup-routes.js
@@ -1,3 +1,43 @@
-// Leo: login, sign up, 
-// George: User, messgae
-// Alix: wine, menu
+const router = require('express').Router();
+const { user } = require('../../models');
+
+router
+  .route('/')
+  .get(async (req, res) => {
+    try {
+      const users = await User.findAll({
+        include: [
+          { model: Post, attributes: ['id', 'title', 'description'] },
+          { model: Comment, attributes: ['id', 'post_id', 'comment'] },
+        ],
+      });
+
+      !users ? res.status(404).send(new Error('Oops!')) : null;
+
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  })
+  .post(async (req, res) => {
+    try {
+      const created = await User.create({
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password,
+      });
+
+      !created ? res.status(400).send(new Error('Oops!')) : null;
+
+      req.session.save(() => {
+        req.session.loggedIn = true;
+        req.session.userId = created.dataValues.id;
+
+        res.status(200).json({
+          message: `Created user with username of ${created.username}`,
+        });
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
