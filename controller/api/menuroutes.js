e7dcf60b70ca3abd034932cd4334bef2f8ce13e2
Alix17 <wildflowerinabox@gmail.com>
2021-11-03 13:28:49 -0700

    added html elements and css elements

--- controller/api/menuroutes.js
+++ controller/api/menuroutes.js
@@ -1,81 +1,81 @@
 const router = require('express').Router();
 const { Menu, Wine, Guests } = require('../../models');
 console.log(Menu);
-// //get the whole menu
-// router.get('/', async (req, res) => {
-//   try {
-//     const menuData = await Menu.findAll();
-//     res.status(200).json(menuData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+//get the whole menu
+router.get('/', async (req, res) => {
+  try {
+    const menuData = await Menu.findAll();
+    res.status(200).json(menuData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// // GET one menu item
-// router.get('/:id', async (req, res) => {
-//     try {
-//       const menuData = await Menu.findByPk(req.params.id, {
-//         include: [
-//           {
-//             model: dish,
-//             attributes: [
-//               'id',
-//               'dish',
-//               'guest_id',
-//               'description',
-//             ],
-//           },
-//         ],
-//       });
-//     } catch (err) {
-// res.status(400).json(err);
-//   }
-//   });
+// GET one menu item
+router.get('/:id', async (req, res) => {
+    try {
+      const menuData = await Menu.findByPk(req.params.id, {
+        include: [
+          {
+            model: dish,
+            attributes: [
+              'id',
+              'dish',
+              'guest_id',
+              'description',
+            ],
+          },
+        ],
+      });
+    } catch (err) {
+res.status(400).json(err);
+  }
+  });
 
-// //post menu item
-//   router.post('/', async (req, res) => {
-//     try {
-//       const menuData = await Menu.create(req.body);
-//       res.status(200).json(menuData);
-//     } catch (err) {
-//       res.status(400).json(err);
-//     }
-//   });
+//post menu item
+  router.post('/', async (req, res) => {
+    try {
+      const menuData = await Menu.create(req.body);
+      res.status(200).json(menuData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
-//   // UPDATE a menu item
-// router.put('/:id', async (req, res) => {
-//   try {
-//     const menuData = await Menu.update(req.body, {
-//       where: {
-//         id: req.params.id,
-//       },
-//     });
-//     if (!menuData[0]) {
-//       res.status(404).json({ message: 'No menu item with this id!' });
-//       return;
-//     }
-//     res.status(200).json(menuData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+  // UPDATE a menu item
+router.put('/:id', async (req, res) => {
+  try {
+    const menuData = await Menu.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!menuData[0]) {
+      res.status(404).json({ message: 'No menu item with this id!' });
+      return;
+    }
+    res.status(200).json(menuData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-//   // DELETE a menu item nobody likes your gluten free rice cakes KAREN
-//   router.delete('/:id', async (req, res) => {
-//     try {
-//       const menuData = await Menu.destroy({
-//         where: {
-//           id: req.params.id,
-//         },
-//       });
-//       if (!menuData) {
-//         res.status(404).json({ message: 'No menu item with this id!' });
-//         return;
-//       }
-//       res.status(200).json(menuData);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
+  // DELETE a menu item nobody likes your gluten free rice cakes KAREN
+  router.delete('/:id', async (req, res) => {
+    try {
+      const menuData = await Menu.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!menuData) {
+        res.status(404).json({ message: 'No menu item with this id!' });
+        return;
+      }
+      res.status(200).json(menuData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
   
   module.exports = router;
