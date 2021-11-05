0ca815aedb55de9b6388b0c9b88b2d4b8b74edf7
Alix17 <wildflowerinabox@gmail.com>
2021-11-05 09:53:13 -0700

    last minute small edits

--- controller/api/menuroutes.js
+++ controller/api/menuroutes.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const { Menu, Wine, User } = require('../../models');
+const router = require("express").Router();
+const { Menu, User } = require("../../models");
 console.log(Menu);
 //get the whole menu
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const menuData = await Menu.findAll();
     res.status(200).json(menuData);
@@ -12,38 +12,33 @@ router.get('/', async (req, res) => {
 });
 
 // GET one menu item
-router.get('/:id', async (req, res) => {
-    try {
-      const menuData = await Menu.findByPk(req.params.id, {
-        include: [
-          {
-            model: dish,
-            attributes: [
-              'id',
-              'dish',
-              'guest_id',
-              'description',
-            ],
-          },
-        ],
-      });
-    } catch (err) {
-res.status(400).json(err);
+router.get("/:id", async (req, res) => {
+  try {
+    const menuData = await Menu.findByPk(req.params.id, {
+      include: [
+        {
+          model: dish,
+          attributes: ["id", "dish", "guest_id", "description"],
+        },
+      ],
+    });
+  } catch (err) {
+    res.status(400).json(err);
   }
-  });
+});
 
 //post menu item
-  router.post('/', async (req, res) => {
-    try {
-      const menuData = await Menu.create(req.body);
-      res.status(200).json(menuData);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+router.post("/", async (req, res) => {
+  try {
+    const menuData = await Menu.create(req.body);
+    res.status(200).json(menuData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-  // UPDATE a menu item
-router.put('/:id', async (req, res) => {
+// UPDATE a menu item
+router.put("/:id", async (req, res) => {
   try {
     const menuData = await Menu.update(req.body, {
       where: {
@@ -51,7 +46,7 @@ router.put('/:id', async (req, res) => {
       },
     });
     if (!menuData[0]) {
-      res.status(404).json({ message: 'No menu item with this id!' });
+      res.status(404).json({ message: "No menu item with this id!" });
       return;
     }
     res.status(200).json(menuData);
@@ -60,22 +55,22 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-  // DELETE a menu item nobody likes your gluten free rice cakes KAREN
-  router.delete('/:id', async (req, res) => {
-    try {
-      const menuData = await Menu.destroy({
-        where: {
-          id: req.params.id,
-        },
-      });
-      if (!menuData) {
-        res.status(404).json({ message: 'No menu item with this id!' });
-        return;
-      }
-      res.status(200).json(menuData);
-    } catch (err) {
-      res.status(500).json(err);
+// DELETE a menu item nobody likes your gluten free rice cakes KAREN
+router.delete("/:id", async (req, res) => {
+  try {
+    const menuData = await Menu.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!menuData) {
+      res.status(404).json({ message: "No menu item with this id!" });
+      return;
     }
-  });
-  
-  module.exports = router;
+    res.status(200).json(menuData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
