b22f1d8a3caef4aec4339793d85f0078d7042723
George Sandoval <gssandoval09@gmail.com>
2021-11-05 00:41:55 -0500

    changed soem code on script, added eventlisteners

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,19 +1,14 @@
 const router = require("express").Router();
 
-const menuroutes = require("./menuroutes");
-
-<<<<<<< HEAD
 const menuroutes = require('./menuroutes');
 // const wineroutes =require('./wine-routes');
 const userroutes = require('./user-routes');
 router.use('/menu', menuroutes);
 // router.use('/wine', wineroutes);
 router.use('/users', userroutes);
-=======
 // const userroutes = require('./user-routes');
 router.use("/menu", menuroutes);
 
 // router.use('/users', userroutes);
->>>>>>> 10fad68e969423615c8794ddde2b59b906146c2e
 
 module.exports = router;
--- public/js/script.js
+++ public/js/script.js
@@ -2,12 +2,9 @@
 const menuFormHandler = async (event) => {
   event.preventDefault();
 
-  const user = document.querySelector("#user").trim();
-  const dish = document.querySelector("#dish").trim();
-  const description = document
-    .querySelector("#description")
-    .ariaValueMax.trim();
-
+  const user = document.getElementById("user");
+  const dish = document.getElementById("dish");
+  const description = document.getElementById("description");
   if (user && dish && description) {
     const response = await fetch("/api/menu", {
       method: "POST",
@@ -15,89 +12,91 @@ const menuFormHandler = async (event) => {
       header: { "Content-Type": "application/json" },
     });
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace("/dashboard");
     } else {
-      alert("Failed to log in");
+      alert("Alert failed");
     }
-  }
-};
+  };
+}
 
-// document.querySelector("#menuAdd").addEventListener("click", menuFormHandler);
+document.getElementById("menuAdd").addEventListener("submit", menuFormHandler);
 
 //event form
 const eventFormHandler = async (event) => {
   event.preventDefault();
 
-  const eventId = document.querySelector("#event_id").trim();
-  const Host = document.querySelector("#host").trim();
-  const eventTheme = document.querySelector("#description").trim();
-  const eventDate = document.querySelector("#date_id").trim();
+  const eventId = document.getElementById("event_id");
+  const Host = document.getElementById("host");
+  const eventTheme = document.getElementById("description");
+  const eventDate = document.getElementById("date_id");
   if (eventId && Host && eventTheme && eventDate) {
     const response = await fetch("/api/event", {
       method: "POST",
       body: JSON.stringify({ eventId, Host, eventTheme, eventDate }),
       header: { "Content-Type": "application/json" },
     });
-    if (response.ok) {
-      document.location.replace("/");
-    } else {
-      alert("Failed to log in");
-    }
+
+    // if (response.ok) {
+    //   document.location.replace("/");
+    // } else {
+    //   alert("Failed to log in");
+    // }
   }
 };
 
-// document.querySelector("#eventAdd").addEventListener("click", eventFormHandler);
+// document.getElementById('EventAdd').addEventListener("submit", eventFormHandler);
 
 // User
-const userFormHandler = async (event) => {
-  event.preventDefault();
 
-  const email = document.querySelector("#email").trim();
+// const userFormHandler = async (event) => {
+//   event.preventDefault();
 
-  const user = document.querySelector("#user").trim();
-  if (user && userId) {
-    const password = document.querySelector("#password").trim();
-    if (email && password) {
-      const response = await fetch("/api/user", {
-        method: "POST",
-        body: JSON.stringify({ email, password }),
-        headers: { "Content-Type": "application/json" },
-      });
-      if (response.ok) {
-        document.location.replace("/");
-      } else {
-        alert("Failed to log in");
-        console.log(user);
-        console.log(password);
-      }
-    }
-  }
-};
+//   const email = document.querySelector("#email").trim();
 
-// document.querySelector("#userAdd").addEventListener("click", userFormHandler);
+//   const user = document.querySelector("#user").trim();
+//   if (user && userId) {
+//     const password = document.querySelector("#password").trim();
+//     if (email && password) {
+//       const response = await fetch("/api/user", {
+//         method: "POST",
+//         body: JSON.stringify({ email, password }),
+//         headers: { "Content-Type": "application/json" },
+//       });
+//       if (response.ok) {
+//         document.location.replace("/");
+//       } else {
+//         alert("Failed to log in");
+//         console.log(user);
+//         console.log(password);
+//       }
+//     }
+//   }
+// };
 
-// document.querySelector("#login").addEventListener("click", userFromHandler);
+// // document.querySelector("#userAdd").addEventListener("click", userFormHandler);
 
-// Guest
+// // document.querySelector("#login").addEventListener("click", userFromHandler);
 
-const guestFormHandler = async (event) => {
-  event.preventDefault();
+// // Guest
 
-  const guest = document.querySelector("#user_id").trim();
+// const guestFormHandler = async (event) => {
+//   event.preventDefault();
 
-  const email = document.querySelector("#email").trim();
-  if (guest && email) {
-    const response = await fetch("/api/user", {
-      method: "POST",
-      body: JSON.stringify({ guest }),
-      headers: { "Content-Type": "application/json" },
-    });
-    if (response.ok) {
-      document.location.replace("/");
-    } else {
-      alert("Failed to log in");
-    }
-  }
-};
+//   const guest = document.querySelector("#user_id").trim();
 
-// document.querySelector("#GuestAdd").addEventListener("click", guestFormHandler);
+//   const email = document.querySelector("#email").trim();
+//   if (guest && email) {
+//     const response = await fetch("/api/user", {
+//       method: "POST",
+//       body: JSON.stringify({ guest }),
+//       headers: { "Content-Type": "application/json" },
+//     });
+//     if (response.ok) {
+//       document.location.replace("/");
+//     } else {
+//       alert("Failed to log in");
+//     }
+//   }
+// };
+
+// // document.querySelector("#GuestAdd").addEventListener("click", guestFormHandler);
